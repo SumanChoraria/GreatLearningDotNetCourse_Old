75ed1c306ca6fdabedfdbece1d007b2d1dd78ecb
SudheerMekala <sudheer.kumar.mekala@gmail.com>
2021-12-14 21:58:50 +0530

    Creating a new project for repository layer

---
+++ SumanChoraria_Sprint2/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SumanChoraria_Sprint2
+{
+    public class Class1
+    {
+    }
+}
--- SumanChoraria_Sprint2/Repositories/IBaseRepository.cs
+++ SumanChoraria_Sprint2/Repositories/IBaseRepository.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SumanChoraria_Sprint1.Models;
 using System;
